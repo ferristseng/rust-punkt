7cdb340369c784dd50a39b7b07909966d6a9af3e
Ferris Tseng <ferristseng@gmail.com>
2015-01-15 12:29:59 -0500

    added bunch of docs, updated where determining token flags occurs for training tokens, updated example project

--- example/main.rs
+++ example/main.rs
@@ -12,30 +12,31 @@ fn main() {
 
     trainer.train(include_str!("../test/raw/npr-article-01.txt"));
     trainer.train(include_str!("../test/raw/ny-times-article-01.txt"));
+    trainer.train(include_str!("../test/raw/pride-and-prejudice.txt"));
     trainer.finalize();
   }
 
-  println!("==Abbreviations==");
+  println!("==Abbreviations ({})==", data.abbrevs_len());
 
   for ab in data.abbrevs_iter() {
     println!("{:?}", ab);
   }
 
-  println!("==Sentence Starters==");
+  println!("==Sentence Starters ({})==", data.sentence_starters_len());
 
   for ss in data.sentence_starters_iter() {
     println!("{:?}", ss);
   }
 
-  println!("==Collocations==");
+  println!("==Collocations ({})==", data.collocations_len());
 
   for (l, r) in data.collocations_iter() {
     println!("({:?}, {:?})", l, r);
   }
 
-  /*
-  println!("==Orthographic Context==");
+  println!("==Orthographic Context ({})==", data.orthographic_context_len());
 
+  /*
   for (s, ctxt) in data.orthographic_context_iter() { 
     println!("{:?}, {:?}", ctxt, s);
   }
